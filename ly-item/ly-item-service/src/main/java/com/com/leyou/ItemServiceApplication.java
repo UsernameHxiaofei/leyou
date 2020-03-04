677a44965b41166d29dda69202b57edb6bfcbb41
824559HUjunfei <junfeihu375@gmail.com>
2020-03-04 15:48:55 +0800

    -m创建项目

---
+++ ly-item/ly-item-service/src/main/java/com/com/leyou/ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ItemServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+}
